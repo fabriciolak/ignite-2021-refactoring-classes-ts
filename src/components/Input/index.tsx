0dfca40117df709aa04c0d3a38ff40a8601216d3
Fabrício <fabriciolak12@gmail.com>
2022-12-23 13:00:46 -0300

    refactor(components): refactor to functional components

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,13 +3,21 @@ import {
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+import { IconType } from 'react-icons/lib';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string
+  icon: IconType
+}
+
+function Input({ name, icon: Icon, ...rest }: InputProps) {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -24,7 +32,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    setIsFilled(!!inputRef.current);
   }, []);
 
   useEffect(() => {
@@ -42,7 +50,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       <input
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
-        defaultValue={defaultValue}
+        value={defaultValue}
         ref={inputRef}
         {...rest}
       />
